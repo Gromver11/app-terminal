764bb19462b041d67ccbf56b38009695040b0c48
Ivan Shirobokov <spirit-1993@list.ru>
2020-10-11 17:05:02 +0400

    Добавляет @types/styled-components и глобальные стили

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,23 @@
-
 import Layout from "../components/Layout";
 import OperatorList from "../components/OperatorList";
 import { operatorListSetup } from "../utils/config";
+import { createGlobalStyle } from "styled-components";
 
+const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+`;
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Чтобы пополнить баланс, выберите оператора из списка</h1>
-    <OperatorList items={operatorListSetup} />
-  </Layout>
+  <>
+    <GlobalStyle />
+    <Layout title="Home | Next.js + TypeScript Example">
+      <h1>Чтобы пополнить баланс, выберите оператора из списка</h1>
+      <OperatorList items={operatorListSetup} />
+    </Layout>
+  </>
 );
 
 export default IndexPage;
