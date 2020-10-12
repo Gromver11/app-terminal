4fb9fe0d85015d0282425c7d097756a7d84e40d1
Ivan Shirobokov <spirit-1993@list.ru>
2020-10-12 16:12:10 +0400

    добавляет ssr для styled-components

--- components/OperatorList.tsx
+++ components/OperatorList.tsx
@@ -1,19 +1,24 @@
 import * as React from "react";
-import  OperatorListItem from "./OperatorListItem";
+import OperatorListItem from "./OperatorListItem";
 import { Operator } from "../interfaces";
+import styled from "styled-components";
 
 type OperatorListProps = {
   items: Operator[];
 };
 
+const OperatorListStyled = styled.ul`
+  list-style: none;
+`;
+
 const OperatorList: React.FC<OperatorListProps> = ({ items }) => (
-  <ul>
+  <OperatorListStyled>
     {items.map((item) => (
       <li key={item.id}>
         <OperatorListItem data={item} />
       </li>
     ))}
-  </ul>
+  </OperatorListStyled>
 );
 
 export default OperatorList;
